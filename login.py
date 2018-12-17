d25793f7eb07e3611343d1b9158ff1841969c497
dopiotrko <dopiotrko@o2.pl>
2018-12-17 13:55:45 +0100

    cleaning the code

--- login.py
+++ login.py
@@ -1,61 +1,33 @@
-import constants
-import oauth2
-import urllib.parse as urlparse
-import json
 from user import User
 from database import Database
+from twitter_utils import get_request_token, get_oauth_verifier, get_access_token
 
-
+# there is nothing interested in that database, I have it for learning only, so do not bother with accessing it
 Database.initialise(user='jagxthtr',
                     password='puasVa9LZh0D58Dy7Z-h8VdmCppGNbko',
                     database='jagxthtr',
                     host='baasu.db.elephantsql.com',
                     port=5432)
-consumer = oauth2.Consumer(constants.CONSUMER_KEY, constants.CONSUMER_SECRET)
+
 user_email = input('Enter your email: ')
 user = User.load_from_db_by_email(user_email)
+
 if user is None:
-    client = oauth2.Client(consumer)
 
-    response, content = client.request(constants.REQUEST_TOKEN_URL, 'POST')
-    if response.status is not 200:
-        print("An error occurred")
+    request_token = get_request_token()
 
-    request_token = dict(urlparse.parse_qsl(content.decode('UTF-8')))
+    oauth_verifier = get_oauth_verifier(request_token)
 
-    print('Go to the following site in your browser: ')
-    print("{}?oauth_token={}".format(constants.AUTHORIZATION_URL, request_token['oauth_token']))
-
-    oauth_verifier = input('What is the PIN? ')
-
-    token = oauth2.Token(request_token['oauth_token'], request_token['oauth_token_secret'])
-    token.set_verifier(oauth_verifier)
-    client = oauth2.Client(consumer, token)
-
-    response, content = client.request(constants.ACCESS_TOKEN_URL, 'POST')
-    if response.status is not 200:
-        pass
-    access_token = dict(urlparse.parse_qsl(content.decode('UTF-8')))
-
-    print(access_token)
+    access_token = get_access_token(request_token, oauth_verifier)
 
     first_name = input('Name: ')
     last_name = input('Surname: ')
 
     user = User(user_email, first_name, last_name, access_token['oauth_token'], access_token['oauth_token_secret'], None)
-
     user.save_to_db()
 
-print('oauth_token = {},\noauth_token_success = {}'.format(user.oauth_token, user.oauth_token_secret))
-authorized_token = oauth2.Token(user.oauth_token, user.oauth_token_secret)
-authorized_client = oauth2.Client(consumer, authorized_token)
+tweets = user.twitter_request('https://api.twitter.com/1.1/search/tweets.json?q=computers+filter:images')
 
-response, content = authorized_client.request('https://api.twitter.com/1.1/search/tweets.json'
-                                              '?q=computers+filter:images', 'GET')
-if response.status is not 200:
-    print('Error: response.status = {}'.format(response.status))
-
-tweets = json.loads(content.decode('UTF-8'))
 for tweet in tweets['statuses']:
     print(tweet['text'])
 print('end')
--- user.py
+++ user.py
@@ -1,4 +1,8 @@
+import oauth2
+import json
 from database import CursorFromConnectionFromPool
+from twitter_utils import consumer
+
 
 
 class User:
@@ -11,7 +15,13 @@ class User:
         self.oauth_token_secret = oauth_token_secret
 
     def __repr__(self):
-        return '<User {}>'.format(self.email)
+        return '<Class User object:\n' \
+               'email: {}\n' \
+               'name: {} {}\n' \
+               'id: {}\n' \
+               'token: {}\n' \
+               'token secret: {}>'.format(self.email, self.first_name, self.last_name, self.id,
+                                          self.oauth_token, self.oauth_token_secret)
 
     def save_to_db(self):
         with CursorFromConnectionFromPool() as cursor:
@@ -25,5 +35,18 @@ class User:
             cursor.execute('SELECT * FROM users WHERE users.email = %s LIMIT 1', (email,))
             data = cursor.fetchone()
             if data is not None:
-                return cls(email=data[1], first_name=data[2], last_name=data[3]
+                return cls(email=data[3], first_name=data[1], last_name=data[2]
                            , oauth_token=data[4], oauth_token_secret=data[5], id=data[0])
+
+    def twitter_request(self, url, verb='GET'):
+        authorized_token = oauth2.Token(self.oauth_token, self.oauth_token_secret)
+        authorized_client = oauth2.Client(consumer, authorized_token)
+
+        response, content = authorized_client.request(url, verb)
+        if response.status is not 200:
+            print('Error: response.status = {}'.format(response.status))
+            print(response)
+            print(content)
+
+        return json.loads(content.decode('utf-8'))
+
